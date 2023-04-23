db534f0f334bbac666d5cb451fe2e04026f77fc4
Anthony Chen <50970130+achen318@users.noreply.github.com>
2023-04-23 19:52:37 -0400

    Update week of 4/24 schedule

--- pages/api/weekly-schedule.js
+++ pages/api/weekly-schedule.js
@@ -15,7 +15,7 @@ export default function handler(req, res) {
     scheduleType: 'week',
     days: [
       {
-        day: 'April 17, 2023',
+        day: 'April 24, 2023',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -132,12 +132,12 @@ export default function handler(req, res) {
             }
           ]
         },
-        block: 'B1',
-        testing: 'No Testing',
+        block: 'B',
+        testing: 'Science Testing',
         announcement: null
       },
       {
-        day: 'April 18, 2023',
+        day: 'April 25, 2023',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -259,11 +259,16 @@ export default function handler(req, res) {
         announcement: null
       },
       {
-        day: 'April 19, 2023',
+        day: 'April 26, 2023',
         bell: {
-          scheduleType: 'conference',
-          scheduleName: 'Conference Schedule',
+          scheduleType: 'regular',
+          scheduleName: 'Regular Schedule',
           schedule: [
+            {
+              name: 'Before School',
+              startTime: '0:00',
+              duration: 420
+            },
             {
               name: 'Before Period 1',
               startTime: '7:00',
@@ -272,116 +277,111 @@ export default function handler(req, res) {
             {
               name: 'Period 1',
               startTime: '8:00',
-              duration: 37
+              duration: 41
             },
             {
               name: 'Before Period 2',
-              startTime: '8:37',
+              startTime: '8:41',
               duration: 4
             },
             {
               name: 'Period 2',
-              startTime: '8:41',
-              duration: 37
+              startTime: '8:45',
+              duration: 41
             },
             {
               name: 'Before Period 3',
-              startTime: '9:18',
-              duration: 4
+              startTime: '9:26',
+              duration: 5
             },
             {
               name: 'Period 3',
-              startTime: '9:22',
-              duration: 37
+              startTime: '9:31',
+              duration: 44
             },
             {
               name: 'Before Period 4',
-              startTime: '9:59',
-              duration: 4
+              startTime: '10:15',
+              duration: 5
             },
             {
               name: 'Period 4',
-              startTime: '10:03',
-              duration: 37
+              startTime: '10:20',
+              duration: 41
             },
             {
               name: 'Before Period 5',
-              startTime: '10:40',
-              duration: 4
+              startTime: '11:01',
+              duration: 5
             },
             {
               name: 'Period 5',
-              startTime: '10:44',
-              duration: 37
+              startTime: '11:06',
+              duration: 41
             },
             {
               name: 'Before Period 6',
-              startTime: '11:21',
-              duration: 4
+              startTime: '11:47',
+              duration: 5
             },
             {
               name: 'Period 6',
-              startTime: '11:25',
-              duration: 37
+              startTime: '11:52',
+              duration: 41
             },
             {
               name: 'Before Period 7',
-              startTime: '12:02',
-              duration: 4
+              startTime: '12:33',
+              duration: 5
             },
             {
               name: 'Period 7',
-              startTime: '12:06',
-              duration: 37
+              startTime: '12:38',
+              duration: 41
             },
             {
               name: 'Before Period 8',
-              startTime: '12:43',
-              duration: 4
+              startTime: '13:19',
+              duration: 5
             },
             {
               name: 'Period 8',
-              startTime: '12:47',
-              duration: 37
+              startTime: '13:24',
+              duration: 41
             },
             {
               name: 'Before Period 9',
-              startTime: '13:24',
-              duration: 4
-            },
-            {
-              name: 'Period 9',
-              startTime: '13:28',
-              duration: 37
-            },
-            {
-              name: 'Before Period 10',
               startTime: '14:05',
               duration: 4
             },
             {
-              name: 'Period 10',
+              name: 'Period 9',
               startTime: '14:09',
-              duration: 37
+              duration: 41
             },
             {
-              name: 'Before Meeting',
-              startTime: '14:46',
+              name: 'Before Period 10',
+              startTime: '14:50',
               duration: 4
             },
             {
-              name: 'Meeting',
-              startTime: '14:50',
-              duration: 40
+              name: 'Period 10',
+              startTime: '14:54',
+              duration: 41
+            },
+            {
+              name: 'After School',
+              startTime: '15:35',
+              duration: 504
             }
           ]
         },
-        block: 'B2',
-        testing: 'WL, ELA, CS, SS, & Health Testing',
-        announcement: 'Spring Choral Concert at 6 PM.'
+        block: 'B1',
+        testing: 'WL, ELA, & Health Testing',
+        announcement: null
       },
       {
-        day: 'April 20, 2023',
+        day: 'April 27, 2023',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -499,15 +499,15 @@ export default function handler(req, res) {
           ]
         },
         block: 'A2',
-        testing: 'WL, ELA, CS, SS, & Health Testing',
+        testing: 'Math, Music, & Art',
         announcement: null
       },
       {
-        day: 'April 21, 2023',
+        day: 'April 28, 2023',
         bell: null,
-        block: null,
-        testing: 'No Testing',
-        announcement: null
+        block: 'B2',
+        testing: 'CS, SS, & Tech Testing',
+        announcement: 'Spring Band Concert'
       }
     ]
   });
