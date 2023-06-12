4d13ce228828b19022ab4b5222a144126217e637
Anthony Chen <50970130+achen318@users.noreply.github.com>
2023-06-11 22:26:45 -0400

    Update week of 6/12 schedule

--- pages/api/weekly-schedule.js
+++ pages/api/weekly-schedule.js
@@ -15,7 +15,7 @@ export default function handler(req, res) {
     scheduleType: 'week',
     days: [
       {
-        day: 'June 5, 2023',
+        day: 'June 12, 2023',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -133,11 +133,11 @@ export default function handler(req, res) {
           ]
         },
         block: 'B2',
-        testing: 'Science Testing',
+        testing: 'Makeup Exams',
         announcement: null
       },
       {
-        day: 'June 6, 2023',
+        day: 'June 13, 2023',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -254,260 +254,30 @@ export default function handler(req, res) {
             }
           ]
         },
-        block: 'A1',
-        testing: 'WL, ELA & Health Testing',
+        block: 'A',
+        testing: 'Makeup Exams',
         announcement: null
       },
       {
-        day: 'June 7, 2023',
-        bell: {
-          scheduleType: 'regular',
-          scheduleName: 'Regular Schedule',
-          schedule: [
-            {
-              name: 'Before School',
-              startTime: '0:00',
-              duration: 420
-            },
-            {
-              name: 'Before Period 1',
-              startTime: '7:00',
-              duration: 60
-            },
-            {
-              name: 'Period 1',
-              startTime: '8:00',
-              duration: 41
-            },
-            {
-              name: 'Before Period 2',
-              startTime: '8:41',
-              duration: 4
-            },
-            {
-              name: 'Period 2',
-              startTime: '8:45',
-              duration: 41
-            },
-            {
-              name: 'Before Period 3',
-              startTime: '9:26',
-              duration: 5
-            },
-            {
-              name: 'Period 3',
-              startTime: '9:31',
-              duration: 44
-            },
-            {
-              name: 'Before Period 4',
-              startTime: '10:15',
-              duration: 5
-            },
-            {
-              name: 'Period 4',
-              startTime: '10:20',
-              duration: 41
-            },
-            {
-              name: 'Before Period 5',
-              startTime: '11:01',
-              duration: 5
-            },
-            {
-              name: 'Period 5',
-              startTime: '11:06',
-              duration: 41
-            },
-            {
-              name: 'Before Period 6',
-              startTime: '11:47',
-              duration: 5
-            },
-            {
-              name: 'Period 6',
-              startTime: '11:52',
-              duration: 41
-            },
-            {
-              name: 'Before Period 7',
-              startTime: '12:33',
-              duration: 5
-            },
-            {
-              name: 'Period 7',
-              startTime: '12:38',
-              duration: 41
-            },
-            {
-              name: 'Before Period 8',
-              startTime: '13:19',
-              duration: 5
-            },
-            {
-              name: 'Period 8',
-              startTime: '13:24',
-              duration: 41
-            },
-            {
-              name: 'Before Period 9',
-              startTime: '14:05',
-              duration: 4
-            },
-            {
-              name: 'Period 9',
-              startTime: '14:09',
-              duration: 41
-            },
-            {
-              name: 'Before Period 10',
-              startTime: '14:50',
-              duration: 4
-            },
-            {
-              name: 'Period 10',
-              startTime: '14:54',
-              duration: 41
-            },
-            {
-              name: 'After School',
-              startTime: '15:35',
-              duration: 504
-            }
-          ]
-        },
-        block: 'B1',
-        testing: 'Math, Music, & Art Testing',
-        announcement: null
-      },
-      {
-        day: 'June 8, 2023',
+        day: 'June 14, 2023',
         bell: null,
         block: null,
         testing: 'No Testing',
-        announcement: 'Camp Stuy'
+        announcement: 'ELA Regents (8:30 am), LE Regents (12:30 pm)'
       },
       {
-        day: 'June 9, 2023',
-        bell: {
-          scheduleType: 'regular',
-          scheduleName: 'Regular Schedule',
-          schedule: [
-            {
-              name: 'Before School',
-              startTime: '0:00',
-              duration: 420
-            },
-            {
-              name: 'Before Period 1',
-              startTime: '7:00',
-              duration: 60
-            },
-            {
-              name: 'Period 1',
-              startTime: '8:00',
-              duration: 41
-            },
-            {
-              name: 'Before Period 2',
-              startTime: '8:41',
-              duration: 4
-            },
-            {
-              name: 'Period 2',
-              startTime: '8:45',
-              duration: 41
-            },
-            {
-              name: 'Before Period 3',
-              startTime: '9:26',
-              duration: 5
-            },
-            {
-              name: 'Period 3',
-              startTime: '9:31',
-              duration: 44
-            },
-            {
-              name: 'Before Period 4',
-              startTime: '10:15',
-              duration: 5
-            },
-            {
-              name: 'Period 4',
-              startTime: '10:20',
-              duration: 41
-            },
-            {
-              name: 'Before Period 5',
-              startTime: '11:01',
-              duration: 5
-            },
-            {
-              name: 'Period 5',
-              startTime: '11:06',
-              duration: 41
-            },
-            {
-              name: 'Before Period 6',
-              startTime: '11:47',
-              duration: 5
-            },
-            {
-              name: 'Period 6',
-              startTime: '11:52',
-              duration: 41
-            },
-            {
-              name: 'Before Period 7',
-              startTime: '12:33',
-              duration: 5
-            },
-            {
-              name: 'Period 7',
-              startTime: '12:38',
-              duration: 41
-            },
-            {
-              name: 'Before Period 8',
-              startTime: '13:19',
-              duration: 5
-            },
-            {
-              name: 'Period 8',
-              startTime: '13:24',
-              duration: 41
-            },
-            {
-              name: 'Before Period 9',
-              startTime: '14:05',
-              duration: 4
-            },
-            {
-              name: 'Period 9',
-              startTime: '14:09',
-              duration: 41
-            },
-            {
-              name: 'Before Period 10',
-              startTime: '14:50',
-              duration: 4
-            },
-            {
-              name: 'Period 10',
-              startTime: '14:54',
-              duration: 41
-            },
-            {
-              name: 'After School',
-              startTime: '15:35',
-              duration: 504
-            }
-          ]
-        },
-        block: 'A2',
-        testing: 'CS, SS, & Tech Testing',
-        announcement: null
+        day: 'June 15, 2023',
+        bell: null,
+        block: null,
+        testing: 'No Testing',
+        announcement: 'Global Regents (8:30 am), Algebra I (12:30 pm)'
+      },
+      {
+        day: 'June 16, 2023',
+        bell: null,
+        block: null,
+        testing: 'No Testing',
+        announcement: 'Chemistry Regents (12:30 pm)'
       }
     ]
   });
