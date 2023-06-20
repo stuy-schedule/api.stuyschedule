2889f54502e5edcf8c6934f6fd41b0d334b6d49c
Anthony Chen <50970130+achen318@users.noreply.github.com>
2023-06-20 10:36:25 -0400

    Update week of 6/19 schedule

--- pages/api/weekly-schedule.js
+++ pages/api/weekly-schedule.js
@@ -15,269 +15,39 @@ export default function handler(req, res) {
     scheduleType: 'week',
     days: [
       {
-        day: 'June 12, 2023',
-        bell: {
-          scheduleType: 'regular',
-          scheduleName: 'Regular Schedule',
-          schedule: [
-            {
-              name: 'Before School',
-              startTime: '0:00',
-              duration: 420
-            },
-            {
-              name: 'Before Period 1',
-              startTime: '7:00',
-              duration: 60
-            },
-            {
-              name: 'Period 1',
-              startTime: '8:00',
-              duration: 41
-            },
-            {
-              name: 'Before Period 2',
-              startTime: '8:41',
-              duration: 4
-            },
-            {
-              name: 'Period 2',
-              startTime: '8:45',
-              duration: 41
-            },
-            {
-              name: 'Before Period 3',
-              startTime: '9:26',
-              duration: 5
-            },
-            {
-              name: 'Period 3',
-              startTime: '9:31',
-              duration: 44
-            },
-            {
-              name: 'Before Period 4',
-              startTime: '10:15',
-              duration: 5
-            },
-            {
-              name: 'Period 4',
-              startTime: '10:20',
-              duration: 41
-            },
-            {
-              name: 'Before Period 5',
-              startTime: '11:01',
-              duration: 5
-            },
-            {
-              name: 'Period 5',
-              startTime: '11:06',
-              duration: 41
-            },
-            {
-              name: 'Before Period 6',
-              startTime: '11:47',
-              duration: 5
-            },
-            {
-              name: 'Period 6',
-              startTime: '11:52',
-              duration: 41
-            },
-            {
-              name: 'Before Period 7',
-              startTime: '12:33',
-              duration: 5
-            },
-            {
-              name: 'Period 7',
-              startTime: '12:38',
-              duration: 41
-            },
-            {
-              name: 'Before Period 8',
-              startTime: '13:19',
-              duration: 5
-            },
-            {
-              name: 'Period 8',
-              startTime: '13:24',
-              duration: 41
-            },
-            {
-              name: 'Before Period 9',
-              startTime: '14:05',
-              duration: 4
-            },
-            {
-              name: 'Period 9',
-              startTime: '14:09',
-              duration: 41
-            },
-            {
-              name: 'Before Period 10',
-              startTime: '14:50',
-              duration: 4
-            },
-            {
-              name: 'Period 10',
-              startTime: '14:54',
-              duration: 41
-            },
-            {
-              name: 'After School',
-              startTime: '15:35',
-              duration: 504
-            }
-          ]
-        },
-        block: 'B2',
-        testing: 'Makeup Exams',
+        day: 'June 20, 2023',
+        bell: null,
+        block: null,
+        testing: 'No Testing',
         announcement: null
       },
       {
-        day: 'June 13, 2023',
-        bell: {
-          scheduleType: 'regular',
-          scheduleName: 'Regular Schedule',
-          schedule: [
-            {
-              name: 'Before School',
-              startTime: '0:00',
-              duration: 420
-            },
-            {
-              name: 'Before Period 1',
-              startTime: '7:00',
-              duration: 60
-            },
-            {
-              name: 'Period 1',
-              startTime: '8:00',
-              duration: 41
-            },
-            {
-              name: 'Before Period 2',
-              startTime: '8:41',
-              duration: 4
-            },
-            {
-              name: 'Period 2',
-              startTime: '8:45',
-              duration: 41
-            },
-            {
-              name: 'Before Period 3',
-              startTime: '9:26',
-              duration: 5
-            },
-            {
-              name: 'Period 3',
-              startTime: '9:31',
-              duration: 44
-            },
-            {
-              name: 'Before Period 4',
-              startTime: '10:15',
-              duration: 5
-            },
-            {
-              name: 'Period 4',
-              startTime: '10:20',
-              duration: 41
-            },
-            {
-              name: 'Before Period 5',
-              startTime: '11:01',
-              duration: 5
-            },
-            {
-              name: 'Period 5',
-              startTime: '11:06',
-              duration: 41
-            },
-            {
-              name: 'Before Period 6',
-              startTime: '11:47',
-              duration: 5
-            },
-            {
-              name: 'Period 6',
-              startTime: '11:52',
-              duration: 41
-            },
-            {
-              name: 'Before Period 7',
-              startTime: '12:33',
-              duration: 5
-            },
-            {
-              name: 'Period 7',
-              startTime: '12:38',
-              duration: 41
-            },
-            {
-              name: 'Before Period 8',
-              startTime: '13:19',
-              duration: 5
-            },
-            {
-              name: 'Period 8',
-              startTime: '13:24',
-              duration: 41
-            },
-            {
-              name: 'Before Period 9',
-              startTime: '14:05',
-              duration: 4
-            },
-            {
-              name: 'Period 9',
-              startTime: '14:09',
-              duration: 41
-            },
-            {
-              name: 'Before Period 10',
-              startTime: '14:50',
-              duration: 4
-            },
-            {
-              name: 'Period 10',
-              startTime: '14:54',
-              duration: 41
-            },
-            {
-              name: 'After School',
-              startTime: '15:35',
-              duration: 504
-            }
-          ]
-        },
-        block: 'A',
-        testing: 'Makeup Exams',
+        day: 'June 21, 2023',
+        bell: null,
+        block: null,
+        testing: 'No Testing',
+        announcement: 'Geometry Regents (8:30 am), LOTE Regents (12:30 pm)'
+      },
+      {
+        day: 'June 22, 2023',
+        bell: null,
+        block: null,
+        testing: 'No Testing',
+        announcement: 'Algebra II Regents (8:30 am)'
+      },
+      {
+        day: 'June 23, 2023',
+        bell: null,
+        block: null,
+        testing: 'No Testing',
+        announcement: 'Physics (8:30 am)'
+      },
+      {
+        day: 'June 24, 2023',
+        bell: null,
+        block: null,
+        testing: 'No Testing',
         announcement: null
-      },
-      {
-        day: 'June 14, 2023',
-        bell: null,
-        block: null,
-        testing: 'No Testing',
-        announcement: 'ELA Regents (8:30 am), LE Regents (12:30 pm)'
-      },
-      {
-        day: 'June 15, 2023',
-        bell: null,
-        block: null,
-        testing: 'No Testing',
-        announcement: 'Global Regents (8:30 am), Algebra I (12:30 pm)'
-      },
-      {
-        day: 'June 16, 2023',
-        bell: null,
-        block: null,
-        testing: 'No Testing',
-        announcement: 'Chemistry Regents (12:30 pm)'
       }
     ]
   });
