ab1febf364e389ffe0cab76b65632ae0d292e20e
Anthony Chen <50970130+achen318@users.noreply.github.com>
2023-06-25 01:12:53 -0400

    Update week of 6/26 schedule (and add last day)

--- pages/api/weekly-schedule.js
+++ pages/api/weekly-schedule.js
@@ -15,39 +15,248 @@ export default function handler(req, res) {
     scheduleType: 'week',
     days: [
       {
-        day: 'June 19, 2023',
-        bell: null,
-        block: null,
+        day: 'June 26, 2023',
+        bell: {
+          scheduleType: 'regular',
+          scheduleName: 'Regular Schedule',
+          schedule: [
+            {
+              name: 'Before School',
+              startTime: '0:00',
+              duration: 420
+            },
+            {
+              name: 'Before Period 1',
+              startTime: '7:00',
+              duration: 60
+            },
+            {
+              name: 'Period 1',
+              startTime: '8:00',
+              duration: 41
+            },
+            {
+              name: 'Before Period 2',
+              startTime: '8:41',
+              duration: 4
+            },
+            {
+              name: 'Period 2',
+              startTime: '8:45',
+              duration: 41
+            },
+            {
+              name: 'Before Period 3',
+              startTime: '9:26',
+              duration: 5
+            },
+            {
+              name: 'Period 3',
+              startTime: '9:31',
+              duration: 44
+            },
+            {
+              name: 'Before Period 4',
+              startTime: '10:15',
+              duration: 5
+            },
+            {
+              name: 'Period 4',
+              startTime: '10:20',
+              duration: 41
+            },
+            {
+              name: 'Before Period 5',
+              startTime: '11:01',
+              duration: 5
+            },
+            {
+              name: 'Period 5',
+              startTime: '11:06',
+              duration: 41
+            },
+            {
+              name: 'Before Period 6',
+              startTime: '11:47',
+              duration: 5
+            },
+            {
+              name: 'Period 6',
+              startTime: '11:52',
+              duration: 41
+            },
+            {
+              name: 'Before Period 7',
+              startTime: '12:33',
+              duration: 5
+            },
+            {
+              name: 'Period 7',
+              startTime: '12:38',
+              duration: 41
+            },
+            {
+              name: 'Before Period 8',
+              startTime: '13:19',
+              duration: 5
+            },
+            {
+              name: 'Period 8',
+              startTime: '13:24',
+              duration: 41
+            },
+            {
+              name: 'Before Period 9',
+              startTime: '14:05',
+              duration: 4
+            },
+            {
+              name: 'Period 9',
+              startTime: '14:09',
+              duration: 41
+            },
+            {
+              name: 'Before Period 10',
+              startTime: '14:50',
+              duration: 4
+            },
+            {
+              name: 'Period 10',
+              startTime: '14:54',
+              duration: 41
+            },
+            {
+              name: 'After School',
+              startTime: '15:35',
+              duration: 504
+            }
+          ]
+        },
+        block: 'A',
         testing: 'No Testing',
-        announcement: null
+        announcement: 'Graduation'
       },
       {
-        day: 'June 20, 2023',
-        bell: null,
-        block: null,
+        day: 'June 27, 2023',
+        bell: {
+          scheduleType: 'lastday',
+          scheduleName: 'Last Day Schedule',
+          schedule: [
+            {
+              name: 'Before Period 1',
+              startTime: '8:00',
+              duration: 60
+            },
+            {
+              name: 'Period 1',
+              startTime: '9:20',
+              duration: 25
+            },
+            {
+              name: 'Before Period 2',
+              startTime: '9:45',
+              duration: 4
+            },
+            {
+              name: 'Period 2',
+              startTime: '9:49',
+              duration: 25
+            },
+            {
+              name: 'Before Period 3',
+              startTime: '10:14',
+              duration: 4
+            },
+            {
+              name: 'Period 3',
+              startTime: '10:18',
+              duration: 25
+            },
+            {
+              name: 'Before Homeroom',
+              startTime: '10:43',
+              duration: 4
+            },
+            {
+              name: 'Homeroom',
+              startTime: '10:47',
+              duration: 40
+            },
+            {
+              name: 'Before Period 4',
+              startTime: '11:27',
+              duration: 4
+            },
+            {
+              name: 'Period 4',
+              startTime: '11:31',
+              duration: 25
+            },
+            {
+              name: 'Before Period 5',
+              startTime: '11:56',
+              duration: 4
+            },
+            {
+              name: 'Period 5',
+              startTime: '12:00',
+              duration: 25
+            },
+            {
+              name: 'Before Period 6',
+              startTime: '12:25',
+              duration: 4
+            },
+            {
+              name: 'Period 6',
+              startTime: '12:29',
+              duration: 25
+            },
+            {
+              name: 'Before Period 7',
+              startTime: '12:54',
+              duration: 4
+            },
+            {
+              name: 'Period 7',
+              startTime: '12:58',
+              duration: 25
+            },
+            {
+              name: 'Before Period 8',
+              startTime: '13:23',
+              duration: 4
+            },
+            {
+              name: 'Period 8',
+              startTime: '13:27',
+              duration: 25
+            },
+            {
+              name: 'Before Period 9',
+              startTime: '13:52',
+              duration: 4
+            },
+            {
+              name: 'Period 9',
+              startTime: '13:56',
+              duration: 25
+            },
+            {
+              name: 'Before Period 10',
+              startTime: '14:21',
+              duration: 4
+            },
+            {
+              name: 'Period 10',
+              startTime: '14:25',
+              duration: 25
+            }
+          ]
+        },
+        block: 'B',
         testing: 'No Testing',
-        announcement: 'Geometry Regents (8:30 am), LOTE Regents (12:30 pm)'
-      },
-      {
-        day: 'June 21, 2023',
-        bell: null,
-        block: null,
-        testing: 'No Testing',
-        announcement: 'Algebra II Regents (8:30 am)'
-      },
-      {
-        day: 'June 22, 2023',
-        bell: null,
-        block: null,
-        testing: 'No Testing',
-        announcement: 'Physics (8:30 am)'
-      },
-      {
-        day: 'June 23, 2023',
-        bell: null,
-        block: null,
-        testing: 'No Testing',
-        announcement: null
+        announcement: 'Last Day of School'
       }
     ]
   });
