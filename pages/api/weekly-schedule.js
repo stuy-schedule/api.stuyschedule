1ef314a4db04eb5cd99eb18ea648d127cd3e232d
Anthony Chen <50970130+achen318@users.noreply.github.com>
2023-09-09 20:36:32 -0400

    Update week of 9/11 schedule

--- pages/api/weekly-schedule.js
+++ pages/api/weekly-schedule.js
@@ -15,120 +15,120 @@ export default function handler(req, res) {
     scheduleType: 'week',
     days: [
       {
-        day: 'September 7, 2023',
+        day: 'September 11, 2023',
         bell: {
-          scheduleType: 'firstweek',
-          scheduleName: 'First Week Schedule',
+          scheduleType: 'regular',
+          scheduleName: 'Regular Schedule',
           schedule: [
             {
-              name: 'Before Homeroom',
-              startTime: '7:30',
-              duration: 60
-            },
-            {
-              name: 'Homeroom',
-              startTime: '8:30',
-              duration: 30
+              name: 'Before School',
+              startTime: '0:00',
+              duration: 420
             },
             {
               name: 'Before Period 1',
-              startTime: '9:00',
-              duration: 5
+              startTime: '7:00',
+              duration: 60
             },
             {
               name: 'Period 1',
-              startTime: '9:05',
-              duration: 30
+              startTime: '8:00',
+              duration: 41
             },
             {
               name: 'Before Period 2',
-              startTime: '9:35',
+              startTime: '8:41',
               duration: 4
             },
             {
               name: 'Period 2',
-              startTime: '9:40',
-              duration: 30
+              startTime: '8:45',
+              duration: 41
             },
             {
               name: 'Before Period 3',
-              startTime: '10:10',
+              startTime: '9:26',
               duration: 5
             },
             {
               name: 'Period 3',
-              startTime: '10:15',
-              duration: 30
+              startTime: '9:31',
+              duration: 44
             },
             {
               name: 'Before Period 4',
-              startTime: '10:45',
+              startTime: '10:15',
               duration: 5
             },
             {
               name: 'Period 4',
-              startTime: '10:50',
-              duration: 30
+              startTime: '10:20',
+              duration: 41
             },
             {
               name: 'Before Period 5',
-              startTime: '11:20',
+              startTime: '11:01',
               duration: 5
             },
             {
               name: 'Period 5',
-              startTime: '11:25',
-              duration: 30
+              startTime: '11:06',
+              duration: 41
             },
             {
               name: 'Before Period 6',
-              startTime: '11:55',
+              startTime: '11:47',
               duration: 5
             },
             {
               name: 'Period 6',
-              startTime: '12:00',
-              duration: 30
+              startTime: '11:52',
+              duration: 41
             },
             {
               name: 'Before Period 7',
-              startTime: '12:30',
+              startTime: '12:33',
               duration: 5
             },
             {
               name: 'Period 7',
-              startTime: '12:35',
-              duration: 30
+              startTime: '12:38',
+              duration: 41
             },
             {
               name: 'Before Period 8',
-              startTime: '13:05',
+              startTime: '13:19',
               duration: 5
             },
             {
               name: 'Period 8',
-              startTime: '13:10',
-              duration: 30
+              startTime: '13:24',
+              duration: 41
             },
             {
               name: 'Before Period 9',
-              startTime: '13:40',
-              duration: 5
+              startTime: '14:05',
+              duration: 4
             },
             {
               name: 'Period 9',
-              startTime: '13:45',
-              duration: 30
+              startTime: '14:09',
+              duration: 41
             },
             {
               name: 'Before Period 10',
-              startTime: '14:15',
-              duration: 5
+              startTime: '14:50',
+              duration: 4
             },
             {
               name: 'Period 10',
-              startTime: '14:20',
-              duration: 30
+              startTime: '14:54',
+              duration: 41
+            },
+            {
+              name: 'After School',
+              startTime: '15:35',
+              duration: 504
             }
           ]
         },
@@ -137,127 +137,493 @@ export default function handler(req, res) {
         announcement: null
       },
       {
-        day: 'September 8, 2023',
+        day: 'September 12, 2023',
         bell: {
-          scheduleType: 'firstweek',
-          scheduleName: 'First Week Schedule',
+          scheduleType: 'regular',
+          scheduleName: 'Regular Schedule',
           schedule: [
             {
-              name: 'Before Homeroom',
-              startTime: '7:30',
-              duration: 60
-            },
-            {
-              name: 'Homeroom',
-              startTime: '8:30',
-              duration: 30
+              name: 'Before School',
+              startTime: '0:00',
+              duration: 420
             },
             {
               name: 'Before Period 1',
-              startTime: '9:00',
-              duration: 5
+              startTime: '7:00',
+              duration: 60
             },
             {
               name: 'Period 1',
-              startTime: '9:05',
-              duration: 30
+              startTime: '8:00',
+              duration: 41
             },
             {
               name: 'Before Period 2',
-              startTime: '9:35',
+              startTime: '8:41',
               duration: 4
             },
             {
               name: 'Period 2',
-              startTime: '9:40',
-              duration: 30
+              startTime: '8:45',
+              duration: 41
             },
             {
               name: 'Before Period 3',
-              startTime: '10:10',
+              startTime: '9:26',
               duration: 5
             },
             {
               name: 'Period 3',
-              startTime: '10:15',
-              duration: 30
+              startTime: '9:31',
+              duration: 44
             },
             {
               name: 'Before Period 4',
-              startTime: '10:45',
+              startTime: '10:15',
               duration: 5
             },
             {
               name: 'Period 4',
-              startTime: '10:50',
-              duration: 30
+              startTime: '10:20',
+              duration: 41
             },
             {
               name: 'Before Period 5',
-              startTime: '11:20',
+              startTime: '11:01',
               duration: 5
             },
             {
               name: 'Period 5',
-              startTime: '11:25',
-              duration: 30
+              startTime: '11:06',
+              duration: 41
             },
             {
               name: 'Before Period 6',
-              startTime: '11:55',
+              startTime: '11:47',
               duration: 5
             },
             {
               name: 'Period 6',
-              startTime: '12:00',
-              duration: 30
+              startTime: '11:52',
+              duration: 41
             },
             {
               name: 'Before Period 7',
-              startTime: '12:30',
+              startTime: '12:33',
               duration: 5
             },
             {
               name: 'Period 7',
-              startTime: '12:35',
-              duration: 30
+              startTime: '12:38',
+              duration: 41
             },
             {
               name: 'Before Period 8',
-              startTime: '13:05',
+              startTime: '13:19',
               duration: 5
             },
             {
               name: 'Period 8',
-              startTime: '13:10',
-              duration: 30
+              startTime: '13:24',
+              duration: 41
             },
             {
               name: 'Before Period 9',
-              startTime: '13:40',
-              duration: 5
+              startTime: '14:05',
+              duration: 4
             },
             {
               name: 'Period 9',
-              startTime: '13:45',
-              duration: 30
+              startTime: '14:09',
+              duration: 41
             },
             {
               name: 'Before Period 10',
-              startTime: '14:15',
-              duration: 5
+              startTime: '14:50',
+              duration: 4
             },
             {
               name: 'Period 10',
-              startTime: '14:20',
-              duration: 30
+              startTime: '14:54',
+              duration: 41
+            },
+            {
+              name: 'After School',
+              startTime: '15:35',
+              duration: 504
             }
           ]
         },
-        block: 'B',
+        block: 'B1',
         testing: 'No Testing',
         announcement: null
-      }
+      },
+      {
+        day: 'September 13, 2023',
+        bell: {
+          scheduleType: 'regular',
+          scheduleName: 'Regular Schedule',
+          schedule: [
+            {
+              name: 'Before School',
+              startTime: '0:00',
+              duration: 420
+            },
+            {
+              name: 'Before Period 1',
+              startTime: '7:00',
+              duration: 60
+            },
+            {
+              name: 'Period 1',
+              startTime: '8:00',
+              duration: 41
+            },
+            {
+              name: 'Before Period 2',
+              startTime: '8:41',
+              duration: 4
+            },
+            {
+              name: 'Period 2',
+              startTime: '8:45',
+              duration: 41
+            },
+            {
+              name: 'Before Period 3',
+              startTime: '9:26',
+              duration: 5
+            },
+            {
+              name: 'Period 3',
+              startTime: '9:31',
+              duration: 44
+            },
+            {
+              name: 'Before Period 4',
+              startTime: '10:15',
+              duration: 5
+            },
+            {
+              name: 'Period 4',
+              startTime: '10:20',
+              duration: 41
+            },
+            {
+              name: 'Before Period 5',
+              startTime: '11:01',
+              duration: 5
+            },
+            {
+              name: 'Period 5',
+              startTime: '11:06',
+              duration: 41
+            },
+            {
+              name: 'Before Period 6',
+              startTime: '11:47',
+              duration: 5
+            },
+            {
+              name: 'Period 6',
+              startTime: '11:52',
+              duration: 41
+            },
+            {
+              name: 'Before Period 7',
+              startTime: '12:33',
+              duration: 5
+            },
+            {
+              name: 'Period 7',
+              startTime: '12:38',
+              duration: 41
+            },
+            {
+              name: 'Before Period 8',
+              startTime: '13:19',
+              duration: 5
+            },
+            {
+              name: 'Period 8',
+              startTime: '13:24',
+              duration: 41
+            },
+            {
+              name: 'Before Period 9',
+              startTime: '14:05',
+              duration: 4
+            },
+            {
+              name: 'Period 9',
+              startTime: '14:09',
+              duration: 41
+            },
+            {
+              name: 'Before Period 10',
+              startTime: '14:50',
+              duration: 4
+            },
+            {
+              name: 'Period 10',
+              startTime: '14:54',
+              duration: 41
+            },
+            {
+              name: 'After School',
+              startTime: '15:35',
+              duration: 504
+            }
+          ]
+        },
+        block: 'A1',
+        testing: 'No Testing',
+        announcement: 'Senior College Night'
+      },
+      {
+        day: 'September 14, 2023',
+        bell: {
+          scheduleType: 'regular',
+          scheduleName: 'Regular Schedule',
+          schedule: [
+            {
+              name: 'Before School',
+              startTime: '0:00',
+              duration: 420
+            },
+            {
+              name: 'Before Period 1',
+              startTime: '7:00',
+              duration: 60
+            },
+            {
+              name: 'Period 1',
+              startTime: '8:00',
+              duration: 41
+            },
+            {
+              name: 'Before Period 2',
+              startTime: '8:41',
+              duration: 4
+            },
+            {
+              name: 'Period 2',
+              startTime: '8:45',
+              duration: 41
+            },
+            {
+              name: 'Before Period 3',
+              startTime: '9:26',
+              duration: 5
+            },
+            {
+              name: 'Period 3',
+              startTime: '9:31',
+              duration: 44
+            },
+            {
+              name: 'Before Period 4',
+              startTime: '10:15',
+              duration: 5
+            },
+            {
+              name: 'Period 4',
+              startTime: '10:20',
+              duration: 41
+            },
+            {
+              name: 'Before Period 5',
+              startTime: '11:01',
+              duration: 5
+            },
+            {
+              name: 'Period 5',
+              startTime: '11:06',
+              duration: 41
+            },
+            {
+              name: 'Before Period 6',
+              startTime: '11:47',
+              duration: 5
+            },
+            {
+              name: 'Period 6',
+              startTime: '11:52',
+              duration: 41
+            },
+            {
+              name: 'Before Period 7',
+              startTime: '12:33',
+              duration: 5
+            },
+            {
+              name: 'Period 7',
+              startTime: '12:38',
+              duration: 41
+            },
+            {
+              name: 'Before Period 8',
+              startTime: '13:19',
+              duration: 5
+            },
+            {
+              name: 'Period 8',
+              startTime: '13:24',
+              duration: 41
+            },
+            {
+              name: 'Before Period 9',
+              startTime: '14:05',
+              duration: 4
+            },
+            {
+              name: 'Period 9',
+              startTime: '14:09',
+              duration: 41
+            },
+            {
+              name: 'Before Period 10',
+              startTime: '14:50',
+              duration: 4
+            },
+            {
+              name: 'Period 10',
+              startTime: '14:54',
+              duration: 41
+            },
+            {
+              name: 'After School',
+              startTime: '15:35',
+              duration: 504
+            }
+          ]
+        },
+        block: 'B2',
+        testing: 'No Testing',
+        announcement: null
+      },
+      {
+        day: 'September 15, 2023',
+        bell: {
+          scheduleType: 'regular',
+          scheduleName: 'Regular Schedule',
+          schedule: [
+            {
+              name: 'Before School',
+              startTime: '0:00',
+              duration: 420
+            },
+            {
+              name: 'Before Period 1',
+              startTime: '7:00',
+              duration: 60
+            },
+            {
+              name: 'Period 1',
+              startTime: '8:00',
+              duration: 41
+            },
+            {
+              name: 'Before Period 2',
+              startTime: '8:41',
+              duration: 4
+            },
+            {
+              name: 'Period 2',
+              startTime: '8:45',
+              duration: 41
+            },
+            {
+              name: 'Before Period 3',
+              startTime: '9:26',
+              duration: 5
+            },
+            {
+              name: 'Period 3',
+              startTime: '9:31',
+              duration: 44
+            },
+            {
+              name: 'Before Period 4',
+              startTime: '10:15',
+              duration: 5
+            },
+            {
+              name: 'Period 4',
+              startTime: '10:20',
+              duration: 41
+            },
+            {
+              name: 'Before Period 5',
+              startTime: '11:01',
+              duration: 5
+            },
+            {
+              name: 'Period 5',
+              startTime: '11:06',
+              duration: 41
+            },
+            {
+              name: 'Before Period 6',
+              startTime: '11:47',
+              duration: 5
+            },
+            {
+              name: 'Period 6',
+              startTime: '11:52',
+              duration: 41
+            },
+            {
+              name: 'Before Period 7',
+              startTime: '12:33',
+              duration: 5
+            },
+            {
+              name: 'Period 7',
+              startTime: '12:38',
+              duration: 41
+            },
+            {
+              name: 'Before Period 8',
+              startTime: '13:19',
+              duration: 5
+            },
+            {
+              name: 'Period 8',
+              startTime: '13:24',
+              duration: 41
+            },
+            {
+              name: 'Before Period 9',
+              startTime: '14:05',
+              duration: 4
+            },
+            {
+              name: 'Period 9',
+              startTime: '14:09',
+              duration: 41
+            },
+            {
+              name: 'Before Period 10',
+              startTime: '14:50',
+              duration: 4
+            },
+            {
+              name: 'Period 10',
+              startTime: '14:54',
+              duration: 41
+            },
+            {
+              name: 'After School',
+              startTime: '15:35',
+              duration: 504
+            }
+          ]
+        },
+        block: 'A2',
+        testing: 'No Testing',
+        announcement: 'Senior Assembly: Voting Rights Forum'
+      },
     ]
   });
 }
