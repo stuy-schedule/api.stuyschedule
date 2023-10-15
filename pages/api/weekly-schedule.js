85cbc016c7c0e67e205e18787fc982dc57cb1e85
Anthony Chen <50970130+achen318@users.noreply.github.com>
2023-10-15 10:04:15 -0400

    Update week of 10/16 schedule

--- pages/api/weekly-schedule.js
+++ pages/api/weekly-schedule.js
@@ -15,7 +15,7 @@ export default function handler(req, res) {
     scheduleType: 'week',
     days: [
       {
-        day: 'October 10, 2023',
+        day: 'October 16, 2023',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -132,12 +132,12 @@ export default function handler(req, res) {
             }
           ]
         },
-        block: 'B1',
+        block: 'B',
         testing: 'Science Testing',
         announcement: null
       },
       {
-        day: 'October 11, 2023',
+        day: 'October 17, 2023',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -255,11 +255,11 @@ export default function handler(req, res) {
           ]
         },
         block: 'A1',
-        testing: 'WL, ELA, & Health Testing',
+        testing: 'Science Testing',
         announcement: null
       },
       {
-        day: 'October 12, 2023',
+        day: 'October 18, 2023',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -376,12 +376,12 @@ export default function handler(req, res) {
             }
           ]
         },
-        block: 'B2',
-        testing: 'Math, Music, & Art Testing',
+        block: 'B1',
+        testing: 'WL, ELA, & Health Testing',
         announcement: null
       },
       {
-        day: 'October 13, 2023',
+        day: 'October 19, 2023',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -499,6 +499,128 @@ export default function handler(req, res) {
           ]
         },
         block: 'A2',
+        testing: 'Math, Music, & Art Testing',
+        announcement: 'End of MP1'
+      },
+      {
+        day: 'October 20, 2023',
+        bell: {
+          scheduleType: 'regular',
+          scheduleName: 'Regular Schedule',
+          schedule: [
+            {
+              name: 'Before School',
+              startTime: '0:00',
+              duration: 420
+            },
+            {
+              name: 'Before Period 1',
+              startTime: '7:00',
+              duration: 60
+            },
+            {
+              name: 'Period 1',
+              startTime: '8:00',
+              duration: 41
+            },
+            {
+              name: 'Before Period 2',
+              startTime: '8:41',
+              duration: 4
+            },
+            {
+              name: 'Period 2',
+              startTime: '8:45',
+              duration: 41
+            },
+            {
+              name: 'Before Period 3',
+              startTime: '9:26',
+              duration: 5
+            },
+            {
+              name: 'Period 3',
+              startTime: '9:31',
+              duration: 44
+            },
+            {
+              name: 'Before Period 4',
+              startTime: '10:15',
+              duration: 5
+            },
+            {
+              name: 'Period 4',
+              startTime: '10:20',
+              duration: 41
+            },
+            {
+              name: 'Before Period 5',
+              startTime: '11:01',
+              duration: 5
+            },
+            {
+              name: 'Period 5',
+              startTime: '11:06',
+              duration: 41
+            },
+            {
+              name: 'Before Period 6',
+              startTime: '11:47',
+              duration: 5
+            },
+            {
+              name: 'Period 6',
+              startTime: '11:52',
+              duration: 41
+            },
+            {
+              name: 'Before Period 7',
+              startTime: '12:33',
+              duration: 5
+            },
+            {
+              name: 'Period 7',
+              startTime: '12:38',
+              duration: 41
+            },
+            {
+              name: 'Before Period 8',
+              startTime: '13:19',
+              duration: 5
+            },
+            {
+              name: 'Period 8',
+              startTime: '13:24',
+              duration: 41
+            },
+            {
+              name: 'Before Period 9',
+              startTime: '14:05',
+              duration: 4
+            },
+            {
+              name: 'Period 9',
+              startTime: '14:09',
+              duration: 41
+            },
+            {
+              name: 'Before Period 10',
+              startTime: '14:50',
+              duration: 4
+            },
+            {
+              name: 'Period 10',
+              startTime: '14:54',
+              duration: 41
+            },
+            {
+              name: 'After School',
+              startTime: '15:35',
+              duration: 504
+            }
+          ]
+        },
+        block: 'B2',
         testing: 'CS, SS, & Technology Testing',
         announcement: null
       }
