f8b7758060b387bb415add51d6a9594d6dd4946d
Anthony Chen <50970130+achen318@users.noreply.github.com>
2023-10-29 20:45:26 -0400

    Update week of 10/30 schedule

--- pages/api/weekly-schedule.js
+++ pages/api/weekly-schedule.js
@@ -15,7 +15,7 @@ export default function handler(req, res) {
     scheduleType: 'week',
     days: [
       {
-        day: 'October 23, 2023',
+        day: 'October 30, 2023',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -132,12 +132,134 @@ export default function handler(req, res) {
             }
           ]
         },
-        block: 'A',
+        block: 'B',
         testing: 'Science Testing',
         announcement: null
       },
       {
-        day: 'October 24, 2023',
+        day: 'October 31, 2023',
+        bell: {
+          scheduleType: 'extendedhomeroom',
+          scheduleName: 'Extended Homeroom Schedule',
+          schedule: [
+            {
+              name: 'Before Period 1',
+              startTime: '7:00',
+              duration: 60
+            },
+            {
+              name: 'Period 1',
+              startTime: '8:00',
+              duration: 40
+            },
+            {
+              name: 'Before Period 2',
+              startTime: '8:40',
+              duration: 4
+            },
+            {
+              name: 'Period 2',
+              startTime: '8:44',
+              duration: 39
+            },
+            {
+              name: 'Before Period 3',
+              startTime: '9:23',
+              duration: 4
+            },
+            {
+              name: 'Period 3',
+              startTime: '9:27',
+              duration: 39
+            },
+            {
+              name: 'Before Homeroom',
+              startTime: '10:06',
+              duration: 4
+            },
+            {
+              name: 'Homeroom',
+              startTime: '10:10',
+              duration: 22
+            },
+            {
+              name: 'Before Period 4',
+              startTime: '10:32',
+              duration: 4
+            },
+            {
+              name: 'Period 4',
+              startTime: '10:36',
+              duration: 39
+            },
+            {
+              name: 'Before Period 5',
+              startTime: '11:15',
+              duration: 4
+            },
+            {
+              name: 'Period 5',
+              startTime: '11:19',
+              duration: 39
+            },
+            {
+              name: 'Before Period 6',
+              startTime: '11:58',
+              duration: 4
+            },
+            {
+              name: 'Period 6',
+              startTime: '12:02',
+              duration: 39
+            },
+            {
+              name: 'Before Period 7',
+              startTime: '12:41',
+              duration: 4
+            },
+            {
+              name: 'Period 7',
+              startTime: '12:45',
+              duration: 39
+            },
+            {
+              name: 'Before Period 8',
+              startTime: '13:24',
+              duration: 4
+            },
+            {
+              name: 'Period 8',
+              startTime: '13:28',
+              duration: 39
+            },
+            {
+              name: 'Before Period 9',
+              startTime: '14:07',
+              duration: 4
+            },
+            {
+              name: 'Period 9',
+              startTime: '14:11',
+              duration: 39
+            },
+            {
+              name: 'Before Period 10',
+              startTime: '14:50',
+              duration: 5
+            },
+            {
+              name: 'Period 10',
+              startTime: '14:55',
+              duration: 40
+            }
+          ]
+        },
+        block: 'A1',
+        testing: 'Science Testing',
+        announcement: 'Distribution of Boograms, Report Card Distribution'
+      },
+      {
+        day: 'November 1, 2023',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -255,255 +377,11 @@ export default function handler(req, res) {
           ]
         },
         block: 'B1',
-        testing: 'Science Testing',
-        announcement: null
-      },
-      {
-        day: 'October 25, 2023',
-        bell: {
-          scheduleType: 'regular',
-          scheduleName: 'Regular Schedule',
-          schedule: [
-            {
-              name: 'Before School',
-              startTime: '0:00',
-              duration: 420
-            },
-            {
-              name: 'Before Period 1',
-              startTime: '7:00',
-              duration: 60
-            },
-            {
-              name: 'Period 1',
-              startTime: '8:00',
-              duration: 41
-            },
-            {
-              name: 'Before Period 2',
-              startTime: '8:41',
-              duration: 4
-            },
-            {
-              name: 'Period 2',
-              startTime: '8:45',
-              duration: 41
-            },
-            {
-              name: 'Before Period 3',
-              startTime: '9:26',
-              duration: 5
-            },
-            {
-              name: 'Period 3',
-              startTime: '9:31',
-              duration: 44
-            },
-            {
-              name: 'Before Period 4',
-              startTime: '10:15',
-              duration: 5
-            },
-            {
-              name: 'Period 4',
-              startTime: '10:20',
-              duration: 41
-            },
-            {
-              name: 'Before Period 5',
-              startTime: '11:01',
-              duration: 5
-            },
-            {
-              name: 'Period 5',
-              startTime: '11:06',
-              duration: 41
-            },
-            {
-              name: 'Before Period 6',
-              startTime: '11:47',
-              duration: 5
-            },
-            {
-              name: 'Period 6',
-              startTime: '11:52',
-              duration: 41
-            },
-            {
-              name: 'Before Period 7',
-              startTime: '12:33',
-              duration: 5
-            },
-            {
-              name: 'Period 7',
-              startTime: '12:38',
-              duration: 41
-            },
-            {
-              name: 'Before Period 8',
-              startTime: '13:19',
-              duration: 5
-            },
-            {
-              name: 'Period 8',
-              startTime: '13:24',
-              duration: 41
-            },
-            {
-              name: 'Before Period 9',
-              startTime: '14:05',
-              duration: 4
-            },
-            {
-              name: 'Period 9',
-              startTime: '14:09',
-              duration: 41
-            },
-            {
-              name: 'Before Period 10',
-              startTime: '14:50',
-              duration: 4
-            },
-            {
-              name: 'Period 10',
-              startTime: '14:54',
-              duration: 41
-            },
-            {
-              name: 'After School',
-              startTime: '15:35',
-              duration: 504
-            }
-          ]
-        },
-        block: 'A1',
         testing: 'WL, ELA, & Health Testing',
-        announcement: null
+        announcement: 'Fall Open House'
       },
       {
-        day: 'October 26, 2023',
-        bell: {
-          scheduleType: 'regular',
-          scheduleName: 'Regular Schedule',
-          schedule: [
-            {
-              name: 'Before School',
-              startTime: '0:00',
-              duration: 420
-            },
-            {
-              name: 'Before Period 1',
-              startTime: '7:00',
-              duration: 60
-            },
-            {
-              name: 'Period 1',
-              startTime: '8:00',
-              duration: 41
-            },
-            {
-              name: 'Before Period 2',
-              startTime: '8:41',
-              duration: 4
-            },
-            {
-              name: 'Period 2',
-              startTime: '8:45',
-              duration: 41
-            },
-            {
-              name: 'Before Period 3',
-              startTime: '9:26',
-              duration: 5
-            },
-            {
-              name: 'Period 3',
-              startTime: '9:31',
-              duration: 44
-            },
-            {
-              name: 'Before Period 4',
-              startTime: '10:15',
-              duration: 5
-            },
-            {
-              name: 'Period 4',
-              startTime: '10:20',
-              duration: 41
-            },
-            {
-              name: 'Before Period 5',
-              startTime: '11:01',
-              duration: 5
-            },
-            {
-              name: 'Period 5',
-              startTime: '11:06',
-              duration: 41
-            },
-            {
-              name: 'Before Period 6',
-              startTime: '11:47',
-              duration: 5
-            },
-            {
-              name: 'Period 6',
-              startTime: '11:52',
-              duration: 41
-            },
-            {
-              name: 'Before Period 7',
-              startTime: '12:33',
-              duration: 5
-            },
-            {
-              name: 'Period 7',
-              startTime: '12:38',
-              duration: 41
-            },
-            {
-              name: 'Before Period 8',
-              startTime: '13:19',
-              duration: 5
-            },
-            {
-              name: 'Period 8',
-              startTime: '13:24',
-              duration: 41
-            },
-            {
-              name: 'Before Period 9',
-              startTime: '14:05',
-              duration: 4
-            },
-            {
-              name: 'Period 9',
-              startTime: '14:09',
-              duration: 41
-            },
-            {
-              name: 'Before Period 10',
-              startTime: '14:50',
-              duration: 4
-            },
-            {
-              name: 'Period 10',
-              startTime: '14:54',
-              duration: 41
-            },
-            {
-              name: 'After School',
-              startTime: '15:35',
-              duration: 504
-            }
-          ]
-        },
-        block: 'B2',
-        testing: 'Math, Music, & Art Testing',
-        announcement: 'Grades Due 8:00 AM'
-      },
-      {
-        day: 'October 27, 2023',
+        day: 'November 2, 2023',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -621,6 +499,128 @@ export default function handler(req, res) {
           ]
         },
         block: 'A2',
+        testing: 'Math, Music, & Art Testing',
+        announcement: 'Fall Open House'
+      },
+      {
+        day: 'November 3, 2023',
+        bell: {
+          scheduleType: 'regular',
+          scheduleName: 'Regular Schedule',
+          schedule: [
+            {
+              name: 'Before School',
+              startTime: '0:00',
+              duration: 420
+            },
+            {
+              name: 'Before Period 1',
+              startTime: '7:00',
+              duration: 60
+            },
+            {
+              name: 'Period 1',
+              startTime: '8:00',
+              duration: 41
+            },
+            {
+              name: 'Before Period 2',
+              startTime: '8:41',
+              duration: 4
+            },
+            {
+              name: 'Period 2',
+              startTime: '8:45',
+              duration: 41
+            },
+            {
+              name: 'Before Period 3',
+              startTime: '9:26',
+              duration: 5
+            },
+            {
+              name: 'Period 3',
+              startTime: '9:31',
+              duration: 44
+            },
+            {
+              name: 'Before Period 4',
+              startTime: '10:15',
+              duration: 5
+            },
+            {
+              name: 'Period 4',
+              startTime: '10:20',
+              duration: 41
+            },
+            {
+              name: 'Before Period 5',
+              startTime: '11:01',
+              duration: 5
+            },
+            {
+              name: 'Period 5',
+              startTime: '11:06',
+              duration: 41
+            },
+            {
+              name: 'Before Period 6',
+              startTime: '11:47',
+              duration: 5
+            },
+            {
+              name: 'Period 6',
+              startTime: '11:52',
+              duration: 41
+            },
+            {
+              name: 'Before Period 7',
+              startTime: '12:33',
+              duration: 5
+            },
+            {
+              name: 'Period 7',
+              startTime: '12:38',
+              duration: 41
+            },
+            {
+              name: 'Before Period 8',
+              startTime: '13:19',
+              duration: 5
+            },
+            {
+              name: 'Period 8',
+              startTime: '13:24',
+              duration: 41
+            },
+            {
+              name: 'Before Period 9',
+              startTime: '14:05',
+              duration: 4
+            },
+            {
+              name: 'Period 9',
+              startTime: '14:09',
+              duration: 41
+            },
+            {
+              name: 'Before Period 10',
+              startTime: '14:50',
+              duration: 4
+            },
+            {
+              name: 'Period 10',
+              startTime: '14:54',
+              duration: 41
+            },
+            {
+              name: 'After School',
+              startTime: '15:35',
+              duration: 504
+            }
+          ]
+        },
+        block: 'B2',
         testing: 'CS, SS, & Technology Testing',
         announcement: null
       }
