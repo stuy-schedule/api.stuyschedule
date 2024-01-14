7013f8e952d7d769b8e3e35adbb119648aa6b38b
Anthony Chen <50970130+achen318@users.noreply.github.com>
2024-01-14 16:13:16 -0500

    Update week of 1/16 schedule

--- pages/api/weekly-schedule.js
+++ pages/api/weekly-schedule.js
@@ -14,130 +14,130 @@ export default function handler(req, res) {
   res.status(200).json({
     scheduleType: 'week',
     days: [
+      // {
+      //   day: 'January 15, 2024',
+      //   bell: {
+      //     scheduleType: 'regular',
+      //     scheduleName: 'Regular Schedule',
+      //     schedule: [
+      //       {
+      //         name: 'Before School',
+      //         startTime: '0:00',
+      //         duration: 420
+      //       },
+      //       {
+      //         name: 'Before Period 1',
+      //         startTime: '7:00',
+      //         duration: 60
+      //       },
+      //       {
+      //         name: 'Period 1',
+      //         startTime: '8:00',
+      //         duration: 41
+      //       },
+      //       {
+      //         name: 'Before Period 2',
+      //         startTime: '8:41',
+      //         duration: 4
+      //       },
+      //       {
+      //         name: 'Period 2',
+      //         startTime: '8:45',
+      //         duration: 41
+      //       },
+      //       {
+      //         name: 'Before Period 3',
+      //         startTime: '9:26',
+      //         duration: 5
+      //       },
+      //       {
+      //         name: 'Period 3',
+      //         startTime: '9:31',
+      //         duration: 44
+      //       },
+      //       {
+      //         name: 'Before Period 4',
+      //         startTime: '10:15',
+      //         duration: 5
+      //       },
+      //       {
+      //         name: 'Period 4',
+      //         startTime: '10:20',
+      //         duration: 41
+      //       },
+      //       {
+      //         name: 'Before Period 5',
+      //         startTime: '11:01',
+      //         duration: 5
+      //       },
+      //       {
+      //         name: 'Period 5',
+      //         startTime: '11:06',
+      //         duration: 41
+      //       },
+      //       {
+      //         name: 'Before Period 6',
+      //         startTime: '11:47',
+      //         duration: 5
+      //       },
+      //       {
+      //         name: 'Period 6',
+      //         startTime: '11:52',
+      //         duration: 41
+      //       },
+      //       {
+      //         name: 'Before Period 7',
+      //         startTime: '12:33',
+      //         duration: 5
+      //       },
+      //       {
+      //         name: 'Period 7',
+      //         startTime: '12:38',
+      //         duration: 41
+      //       },
+      //       {
+      //         name: 'Before Period 8',
+      //         startTime: '13:19',
+      //         duration: 5
+      //       },
+      //       {
+      //         name: 'Period 8',
+      //         startTime: '13:24',
+      //         duration: 41
+      //       },
+      //       {
+      //         name: 'Before Period 9',
+      //         startTime: '14:05',
+      //         duration: 4
+      //       },
+      //       {
+      //         name: 'Period 9',
+      //         startTime: '14:09',
+      //         duration: 41
+      //       },
+      //       {
+      //         name: 'Before Period 10',
+      //         startTime: '14:50',
+      //         duration: 4
+      //       },
+      //       {
+      //         name: 'Period 10',
+      //         startTime: '14:54',
+      //         duration: 41
+      //       },
+      //       {
+      //         name: 'After School',
+      //         startTime: '15:35',
+      //         duration: 504
+      //       }
+      //     ]
+      //   },
+      //   block: 'A',
+      //   testing: 'Science Testing',
+      //   announcement: null
+      // },
       {
-        day: 'January 8, 2024',
-        bell: {
-          scheduleType: 'regular',
-          scheduleName: 'Regular Schedule',
-          schedule: [
-            {
-              name: 'Before School',
-              startTime: '0:00',
-              duration: 420
-            },
-            {
-              name: 'Before Period 1',
-              startTime: '7:00',
-              duration: 60
-            },
-            {
-              name: 'Period 1',
-              startTime: '8:00',
-              duration: 41
-            },
-            {
-              name: 'Before Period 2',
-              startTime: '8:41',
-              duration: 4
-            },
-            {
-              name: 'Period 2',
-              startTime: '8:45',
-              duration: 41
-            },
-            {
-              name: 'Before Period 3',
-              startTime: '9:26',
-              duration: 5
-            },
-            {
-              name: 'Period 3',
-              startTime: '9:31',
-              duration: 44
-            },
-            {
-              name: 'Before Period 4',
-              startTime: '10:15',
-              duration: 5
-            },
-            {
-              name: 'Period 4',
-              startTime: '10:20',
-              duration: 41
-            },
-            {
-              name: 'Before Period 5',
-              startTime: '11:01',
-              duration: 5
-            },
-            {
-              name: 'Period 5',
-              startTime: '11:06',
-              duration: 41
-            },
-            {
-              name: 'Before Period 6',
-              startTime: '11:47',
-              duration: 5
-            },
-            {
-              name: 'Period 6',
-              startTime: '11:52',
-              duration: 41
-            },
-            {
-              name: 'Before Period 7',
-              startTime: '12:33',
-              duration: 5
-            },
-            {
-              name: 'Period 7',
-              startTime: '12:38',
-              duration: 41
-            },
-            {
-              name: 'Before Period 8',
-              startTime: '13:19',
-              duration: 5
-            },
-            {
-              name: 'Period 8',
-              startTime: '13:24',
-              duration: 41
-            },
-            {
-              name: 'Before Period 9',
-              startTime: '14:05',
-              duration: 4
-            },
-            {
-              name: 'Period 9',
-              startTime: '14:09',
-              duration: 41
-            },
-            {
-              name: 'Before Period 10',
-              startTime: '14:50',
-              duration: 4
-            },
-            {
-              name: 'Period 10',
-              startTime: '14:54',
-              duration: 41
-            },
-            {
-              name: 'After School',
-              startTime: '15:35',
-              duration: 504
-            }
-          ]
-        },
-        block: 'A',
-        testing: 'Science Testing',
-        announcement: null
-      },
-      {
-        day: 'January 9, 2024',
+        day: 'January 16, 2024',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -256,10 +256,10 @@ export default function handler(req, res) {
         },
         block: 'B1',
         testing: 'Science Testing',
-        announcement: null
+        announcement: 'In Class Finals'
       },
       {
-        day: 'January 10, 2024',
+        day: 'January 17, 2024',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -378,10 +378,10 @@ export default function handler(req, res) {
         },
         block: 'A1',
         testing: 'WL, ELA & Health Testing',
-        announcement: 'STC Winter Drama - "Arsenic & Old Lace"'
+        announcement: 'In Class Finals'
       },
       {
-        day: 'January 11, 2024',
+        day: 'January 18, 2024',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -500,10 +500,10 @@ export default function handler(req, res) {
         },
         block: 'B2',
         testing: 'Math, Music & Art Testing',
-        announcement: null
+        announcement: 'In Class Finals'
       },
       {
-        day: 'January 12, 2024',
+        day: 'January 19, 2024',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -622,7 +622,7 @@ export default function handler(req, res) {
         },
         block: 'A2',
         testing: 'CS, SS & Technology Testing',
-        announcement: 'STC Winter Drama - "Arsenic & Old Lace"'
+        announcement: 'In Class Finals'
       }
     ]
   });
