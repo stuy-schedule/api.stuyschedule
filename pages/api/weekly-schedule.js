51de8be9ee2b304d5a52db5147b90bb015b1738e
Anthony Chen <50970130+achen318@users.noreply.github.com>
2024-01-28 14:31:14 -0500

    Update week of 1/29 schedule

--- pages/api/weekly-schedule.js
+++ pages/api/weekly-schedule.js
@@ -15,7 +15,136 @@ export default function handler(req, res) {
     scheduleType: 'week',
     days: [
       {
-        day: 'January 22, 2024',
+        day: 'January 29, 2024',
+        bell: null,
+        block: null,
+        testing: null,
+        announcement: null
+      },
+      {
+        day: 'January 30, 2024',
+        bell: {
+          scheduleType: 'homeroom',
+          scheduleName: 'Homeroom Schedule',
+          schedule: [
+            {
+              name: 'Before Period 1',
+              startTime: '7:00',
+              duration: 60
+            },
+            {
+              name: 'Period 1',
+              startTime: '8:00',
+              duration: 40
+            },
+            {
+              name: 'Before Period 2',
+              startTime: '8:40',
+              duration: 5
+            },
+            {
+              name: 'Period 2',
+              startTime: '8:45',
+              duration: 40
+            },
+            {
+              name: 'Before Period 3',
+              startTime: '9:25',
+              duration: 4
+            },
+            {
+              name: 'Period 3',
+              startTime: '9:29',
+              duration: 40
+            },
+            {
+              name: 'Before Homeroom',
+              startTime: '10:09',
+              duration: 4
+            },
+            {
+              name: 'Homeroom',
+              startTime: '10:13',
+              duration: 12
+            },
+            {
+              name: 'Before Period 4',
+              startTime: '10:25',
+              duration: 5
+            },
+            {
+              name: 'Period 4',
+              startTime: '10:30',
+              duration: 40
+            },
+            {
+              name: 'Before Period 5',
+              startTime: '11:10',
+              duration: 4
+            },
+            {
+              name: 'Period 5',
+              startTime: '11:14',
+              duration: 40
+            },
+            {
+              name: 'Before Period 6',
+              startTime: '11:54',
+              duration: 4
+            },
+            {
+              name: 'Period 6',
+              startTime: '11:58',
+              duration: 40
+            },
+            {
+              name: 'Before Period 7',
+              startTime: '12:42',
+              duration: 5
+            },
+            {
+              name: 'Period 7',
+              startTime: '12:47',
+              duration: 41
+            },
+            {
+              name: 'Before Period 8',
+              startTime: '13:22',
+              duration: 4
+            },
+            {
+              name: 'Period 8',
+              startTime: '13:26',
+              duration: 40
+            },
+            {
+              name: 'Before Period 9',
+              startTime: '14:06',
+              duration: 4
+            },
+            {
+              name: 'Period 9',
+              startTime: '14:10',
+              duration: 40
+            },
+            {
+              name: 'Before Period 10',
+              startTime: '14:50',
+              duration: 5
+            },
+            {
+              name: 'Period 10',
+              startTime: '14:55',
+              duration: 40
+            }
+          ]
+        },
+        block: 'A',
+        testing: 'No Testing',
+        announcement: 'Distribution of Metro Cards'
+      },
+      {
+        day: 'January 31, 2024',
         bell: {
           scheduleType: 'regular',
           scheduleName: 'Regular Schedule',
@@ -133,37 +262,252 @@ export default function handler(req, res) {
           ]
         },
         block: 'B',
-        testing: 'Makeup Exams',
-        announcement: 'End of MP3'
+        testing: 'No Testing',
+        announcement: null
       },
       {
-        day: 'January 23, 2024',
-        bell: null,
-        block: null,
-        testing: 'Geometry Finals (9:30 am - 11:00 am)',
-        announcement: 'ELA Regents, LE Regents'
+        day: 'February 1, 2024',
+        bell: {
+          scheduleType: 'regular',
+          scheduleName: 'Regular Schedule',
+          schedule: [
+            {
+              name: 'Before School',
+              startTime: '0:00',
+              duration: 420
+            },
+            {
+              name: 'Before Period 1',
+              startTime: '7:00',
+              duration: 60
+            },
+            {
+              name: 'Period 1',
+              startTime: '8:00',
+              duration: 41
+            },
+            {
+              name: 'Before Period 2',
+              startTime: '8:41',
+              duration: 4
+            },
+            {
+              name: 'Period 2',
+              startTime: '8:45',
+              duration: 41
+            },
+            {
+              name: 'Before Period 3',
+              startTime: '9:26',
+              duration: 5
+            },
+            {
+              name: 'Period 3',
+              startTime: '9:31',
+              duration: 44
+            },
+            {
+              name: 'Before Period 4',
+              startTime: '10:15',
+              duration: 5
+            },
+            {
+              name: 'Period 4',
+              startTime: '10:20',
+              duration: 41
+            },
+            {
+              name: 'Before Period 5',
+              startTime: '11:01',
+              duration: 5
+            },
+            {
+              name: 'Period 5',
+              startTime: '11:06',
+              duration: 41
+            },
+            {
+              name: 'Before Period 6',
+              startTime: '11:47',
+              duration: 5
+            },
+            {
+              name: 'Period 6',
+              startTime: '11:52',
+              duration: 41
+            },
+            {
+              name: 'Before Period 7',
+              startTime: '12:33',
+              duration: 5
+            },
+            {
+              name: 'Period 7',
+              startTime: '12:38',
+              duration: 41
+            },
+            {
+              name: 'Before Period 8',
+              startTime: '13:19',
+              duration: 5
+            },
+            {
+              name: 'Period 8',
+              startTime: '13:24',
+              duration: 41
+            },
+            {
+              name: 'Before Period 9',
+              startTime: '14:05',
+              duration: 4
+            },
+            {
+              name: 'Period 9',
+              startTime: '14:09',
+              duration: 41
+            },
+            {
+              name: 'Before Period 10',
+              startTime: '14:50',
+              duration: 4
+            },
+            {
+              name: 'Period 10',
+              startTime: '14:54',
+              duration: 41
+            },
+            {
+              name: 'After School',
+              startTime: '15:35',
+              duration: 504
+            }
+          ]
+        },
+        block: 'A2',
+        testing: 'No Testing',
+        announcement: null
       },
       {
-        day: 'January 24, 2024',
-        bell: null,
-        block: null,
-        testing: 'Pre-Calculus Finals (9:30 am - 11:00 am)',
-        announcement:
-          'Geometry Regents, US History Regents, Algebra I Regents, Physics Regents'
-      },
-      {
-        day: 'January 25, 2024',
-        bell: null,
-        block: null,
-        testing: 'Algebra II Finals (9:30 am - 11:00 am)',
-        announcement: 'Global Regents, Algebra II Regents'
-      },
-      {
-        day: 'January 26, 2024',
-        bell: null,
-        block: null,
-        testing: 'AP Computer Science Finals (9:30 am - 11:00 am)',
-        announcement: 'Chemistry Regents; Senior Grades Due 8:00 am'
+        day: 'February 2, 2024',
+        bell: {
+          scheduleType: 'regular',
+          scheduleName: 'Regular Schedule',
+          schedule: [
+            {
+              name: 'Before School',
+              startTime: '0:00',
+              duration: 420
+            },
+            {
+              name: 'Before Period 1',
+              startTime: '7:00',
+              duration: 60
+            },
+            {
+              name: 'Period 1',
+              startTime: '8:00',
+              duration: 41
+            },
+            {
+              name: 'Before Period 2',
+              startTime: '8:41',
+              duration: 4
+            },
+            {
+              name: 'Period 2',
+              startTime: '8:45',
+              duration: 41
+            },
+            {
+              name: 'Before Period 3',
+              startTime: '9:26',
+              duration: 5
+            },
+            {
+              name: 'Period 3',
+              startTime: '9:31',
+              duration: 44
+            },
+            {
+              name: 'Before Period 4',
+              startTime: '10:15',
+              duration: 5
+            },
+            {
+              name: 'Period 4',
+              startTime: '10:20',
+              duration: 41
+            },
+            {
+              name: 'Before Period 5',
+              startTime: '11:01',
+              duration: 5
+            },
+            {
+              name: 'Period 5',
+              startTime: '11:06',
+              duration: 41
+            },
+            {
+              name: 'Before Period 6',
+              startTime: '11:47',
+              duration: 5
+            },
+            {
+              name: 'Period 6',
+              startTime: '11:52',
+              duration: 41
+            },
+            {
+              name: 'Before Period 7',
+              startTime: '12:33',
+              duration: 5
+            },
+            {
+              name: 'Period 7',
+              startTime: '12:38',
+              duration: 41
+            },
+            {
+              name: 'Before Period 8',
+              startTime: '13:19',
+              duration: 5
+            },
+            {
+              name: 'Period 8',
+              startTime: '13:24',
+              duration: 41
+            },
+            {
+              name: 'Before Period 9',
+              startTime: '14:05',
+              duration: 4
+            },
+            {
+              name: 'Period 9',
+              startTime: '14:09',
+              duration: 41
+            },
+            {
+              name: 'Before Period 10',
+              startTime: '14:50',
+              duration: 4
+            },
+            {
+              name: 'Period 10',
+              startTime: '14:54',
+              duration: 41
+            },
+            {
+              name: 'After School',
+              startTime: '15:35',
+              duration: 504
+            }
+          ]
+        },
+        block: 'B2',
+        testing: 'No Testing',
+        announcement: 'Grades Due at 8:00 AM'
       }
     ]
   });
