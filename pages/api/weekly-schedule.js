86cb5b9b3947b09e7e9a225380f9b706c9710eb5
DP1117 <dpark070111@gmail.com>
2024-09-04 20:04:16 -0400

    Update week of 9/1 schedule

--- pages/api/weekly-schedule.js
+++ pages/api/weekly-schedule.js
@@ -28,53 +28,248 @@ export default async function handler(req, res) {
         scheduleType: 'week',
         days: [
             {
-                day: 'June 7, 2024',
-                bell: null,
-                block: null,
+                day: 'Sept 7, 2024',
+                bell: {
+                    scheduleType: 'firstweek',
+                    scheduleName: 'First Week Schedule',
+                    schedule: [
+                      {
+                        name: 'Before Homeroom',
+                        startTime: '7:30',
+                        duration: 60
+                      },
+                      {
+                        name: 'Homeroom',
+                        startTime: '8:30',
+                        duration: 30
+                      },
+                      {
+                        name: 'Before Period 1',
+                        startTime: '9:00',
+                        duration: 5
+                      },
+                      {
+                        name: 'Period 1',
+                        startTime: '9:05',
+                        duration: 30
+                      },
+                      {
+                        name: 'Before Period 2',
+                        startTime: '9:35',
+                        duration: 4
+                      },
+                      {
+                        name: 'Period 2',
+                        startTime: '9:40',
+                        duration: 30
+                      },
+                      {
+                        name: 'Before Period 3',
+                        startTime: '10:10',
+                        duration: 5
+                      },
+                      {
+                        name: 'Period 3',
+                        startTime: '10:15',
+                        duration: 30
+                      },
+                      {
+                        name: 'Before Period 4',
+                        startTime: '10:45',
+                        duration: 5
+                      },
+                      {
+                        name: 'Period 4',
+                        startTime: '10:50',
+                        duration: 30
+                      },
+                      {
+                        name: 'Before Period 5',
+                        startTime: '11:20',
+                        duration: 5
+                      },
+                      {
+                        name: 'Period 5',
+                        startTime: '11:25',
+                        duration: 30
+                      },
+                      {
+                        name: 'Before Period 6',
+                        startTime: '11:55',
+                        duration: 5
+                      },
+                      {
+                        name: 'Period 6',
+                        startTime: '12:00',
+                        duration: 30
+                      },
+                      {
+                        name: 'Before Period 7',
+                        startTime: '12:30',
+                        duration: 5
+                      },
+                      {
+                        name: 'Period 7',
+                        startTime: '12:35',
+                        duration: 30
+                      },
+                      {
+                        name: 'Before Period 8',
+                        startTime: '13:05',
+                        duration: 5
+                      },
+                      {
+                        name: 'Period 8',
+                        startTime: '13:10',
+                        duration: 30
+                      },
+                      {
+                        name: 'Before Period 9',
+                        startTime: '13:40',
+                        duration: 5
+                      },
+                      {
+                        name: 'Period 9',
+                        startTime: '13:45',
+                        duration: 30
+                      },
+                      {
+                        name: 'Before Period 10',
+                        startTime: '14:15',
+                        duration: 5
+                      },
+                      {
+                        name: 'Period 10',
+                        startTime: '14:20',
+                        duration: 30
+                      }
+                    ]
+                  },
+                block: 'A',
                 testing: 'No Testing',
-                announcement: 'Have a great summer!'
+                announcement: 'First day of school!'
             },
             {
-                day: 'July 8, 2024',
-                bell: null,
-                block: null,
+                day: 'Sept 8, 2024',
+                bell: {
+                    scheduleType: 'regular',
+                    scheduleName: 'Regular Schedule',
+                    schedule: [
+                      {
+                        name: 'Before School',
+                        startTime: '0:00',
+                        duration: 420
+                      },
+                      {
+                        name: 'Before Period 1',
+                        startTime: '7:00',
+                        duration: 60
+                      },
+                      {
+                        name: 'Period 1',
+                        startTime: '8:00',
+                        duration: 41
+                      },
+                      {
+                        name: 'Before Period 2',
+                        startTime: '8:41',
+                        duration: 4
+                      },
+                      {
+                        name: 'Period 2',
+                        startTime: '8:45',
+                        duration: 41
+                      },
+                      {
+                        name: 'Before Period 3',
+                        startTime: '9:26',
+                        duration: 5
+                      },
+                      {
+                        name: 'Period 3',
+                        startTime: '9:31',
+                        duration: 44
+                      },
+                      {
+                        name: 'Before Period 4',
+                        startTime: '10:15',
+                        duration: 5
+                      },
+                      {
+                        name: 'Period 4',
+                        startTime: '10:20',
+                        duration: 41
+                      },
+                      {
+                        name: 'Before Period 5',
+                        startTime: '11:01',
+                        duration: 5
+                      },
+                      {
+                        name: 'Period 5',
+                        startTime: '11:06',
+                        duration: 41
+                      },
+                      {
+                        name: 'Before Period 6',
+                        startTime: '11:47',
+                        duration: 5
+                      },
+                      {
+                        name: 'Period 6',
+                        startTime: '11:52',
+                        duration: 41
+                      },
+                      {
+                        name: 'Before Period 7',
+                        startTime: '12:33',
+                        duration: 5
+                      },
+                      {
+                        name: 'Period 7',
+                        startTime: '12:38',
+                        duration: 41
+                      },
+                      {
+                        name: 'Before Period 8',
+                        startTime: '13:19',
+                        duration: 5
+                      },
+                      {
+                        name: 'Period 8',
+                        startTime: '13:24',
+                        duration: 41
+                      },
+                      {
+                        name: 'Before Period 9',
+                        startTime: '14:05',
+                        duration: 4
+                      },
+                      {
+                        name: 'Period 9',
+                        startTime: '14:09',
+                        duration: 41
+                      },
+                      {
+                        name: 'Before Period 10',
+                        startTime: '14:50',
+                        duration: 4
+                      },
+                      {
+                        name: 'Period 10',
+                        startTime: '14:54',
+                        duration: 41
+                      },
+                      {
+                        name: 'After School',
+                        startTime: '15:35',
+                        duration: 504
+                      }
+                    ]
+                  },
+                block: 'B',
                 testing: 'No Testing',
-                announcement: 'Have a great summer!'
-            },
-            {
-                day: 'July 9, 2024',
-                bell: null,
-                block: null,
-                testing: 'No Testing',
-                announcement: 'Have a great summer!'
-            },
-            {
-                day: 'July 10, 2024',
-                bell: null,
-                block: null,
-                testing: 'No Testing',
-                announcement: 'Have a great summer!'
-            },
-            {
-                day: 'July 11, 2024',
-                bell: null,
-                block: null,
-                testing: 'No Testing',
-                announcement: 'Have a great summer!'
-            },
-            {
-                day: 'July 12, 2024',
-                bell: null,
-                block: null,
-                testing: 'No Testing',
-                announcement: 'Have a great summer!'
-            },
-            {
-                day: 'July 13, 2024',
-                bell: null,
-                block: null,
-                testing: 'No Testing',
-                announcement: 'Have a great summer!'
+                announcement: null
             },
         ]
     });
